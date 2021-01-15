b40eb6b6f99c5e7d67c3cf98bba772681fb93163
Greg Schuman <gcschuman@hotmail.com>
2021-01-14 19:52:55 -0800

    Added the highlight and unhighlight tag functions and set up the random select function.

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ textarea.focus()
 // After focusing on the text area, we are adding an event listener. We're having it listen when the user takes their finger off of the key. When that happens, it will create a tag
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    if(e.key === 'Enter'){
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
 })
 
 // Creating a tags function
@@ -22,4 +30,46 @@ function createTags(input) {
         tagEl.innerText = tag
         tagsEl.appendChild(tagEl)
     })
+}
+
+// Random select function
+function randomSelect(){
+    const times = 30
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            removeHighlightTag(randomTag)
+        }, 100)
+    }, 100)
+
+    setTimeout(() => {
+        clearInterval(interval)
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+
+        }, 100)
+    }, times * 100)
+}
+
+// Time is measured in milliseconds, so the 100 mentioned in the above function is 100 milliseconds
+
+// Random tag function
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+// Highlight tags
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+// Remove highlight tags
+function removeHighlightTag(tag){
+    tag.classList.remove('highlight')
 }
