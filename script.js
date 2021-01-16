d6dd0d7c6742914e0329b4db9ed6fe0b6a72b593
Greg Schuman <gcschuman@hotmail.com>
2021-01-15 19:47:14 -0800

    Ovehauled the script and styling.

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Variable declarations, telling the script to get the element by the id mentioned in the htnl
-const tagsEl = document.getElementById('tags');
-const textarea = document.getElementById('textarea');
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
 
-// In textarea.focus, we are focusing on the specific event, in this case, the textarea
 textarea.focus()
 
-// After focusing on the text area, we are adding an event listener. We're having it listen when the user takes their finger off of the key. When that happens, it will create a tag
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
 
@@ -18,12 +15,11 @@ textarea.addEventListener('keyup', (e) => {
     }
 })
 
-// Creating a tags function
 function createTags(input) {
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
-    // The comma allows us to split the tags up by a comma.
-    tagsEl.innerHTML = ''
     
+    tagsEl.innerHTML = ''
+
     tags.forEach(tag => {
         const tagEl = document.createElement('span')
         tagEl.classList.add('tag')
@@ -32,7 +28,6 @@ function createTags(input) {
     })
 }
 
-// Random select function
 function randomSelect() {
     const times = 30
 
@@ -58,20 +53,15 @@ function randomSelect() {
     }, times * 100)
 }
 
-// Time is measured in milliseconds, so the 100 mentioned in the above function is 100 milliseconds
-
-// Random tag function
 function pickRandomTag() {
     const tags = document.querySelectorAll('.tag')
     return tags[Math.floor(Math.random() * tags.length)]
 }
 
-// Highlight tags
 function highlightTag(tag) {
     tag.classList.add('highlight')
 }
 
-// Remove highlight tags
-function removeHighlightTag(tag){
+function unHighlightTag(tag) {
     tag.classList.remove('highlight')
 }
